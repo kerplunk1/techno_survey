2c6799fa4af361436b802ccefa9837877e2f5aad
kerplunk <cynepsonic@gmail.com>
2023-05-01 10:06:48 +0400

    added pip requirements, changed the max length of some fields in models

--- survey/mail_sync/models.py
+++ survey/mail_sync/models.py
@@ -5,9 +5,9 @@ from django.db import models
 class Mails(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
-    login = models.CharField(max_length=25, unique=True)
-    ya_app_pass = models.CharField(max_length=20)
-    tmail_pass = models.CharField(max_length=20)
+    login = models.CharField(max_length=50, unique=True)
+    ya_app_pass = models.CharField(max_length=50)
+    tmail_pass = models.CharField(max_length=50)
     sync = models.BooleanField(null=True)
 
     def __str__(self):
